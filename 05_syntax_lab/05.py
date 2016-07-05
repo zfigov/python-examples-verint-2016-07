bd1bff272d27e0ead7d3a7eed4eced7c86ee05c6
zfigov <zfigov@gmail.com>
2016-07-05 10:14:18 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -2,4 +2,15 @@
 Write a program that randomizes integers in a loop
 until it finds a number that is divisable by: 7, 13 and 15
 """
+# Exercise 5
+# By Zvi Figov
+from random import randint
+
+flag_done = False
+
+while not flag_done:
+    num = randint(1,1000000)
+    if num % 7 ==0 and num % 13 ==0 and num % 15 ==0:
+        flag_done = True
+print "Chosen number = %d" % num
 
