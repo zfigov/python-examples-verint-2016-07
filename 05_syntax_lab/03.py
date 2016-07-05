acaab32e2f291ac49e606bbe8e8fa3d2d7b91063
zfigov <zfigov@gmail.com>
2016-07-05 10:14:11 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -4,4 +4,14 @@ and prints the sum total of its digits.
 For example if the number was: 2345
 The result should be: 14
 """
+# Exercise 3
+# By Zvi Figov
+from random import randint
+
+num = randint(1,10000)
+numstr = str(num)
+acc = 0
+for n in range(0,len(numstr)):
+    acc = acc + int(numstr[n])
+print "The sum of the digits of the number %d is %d" % (num,acc)
 
