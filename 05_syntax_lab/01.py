c818798942449628e7bac6cadb85ba5f487e0823
zfigov <zfigov@gmail.com>
2016-07-05 10:14:04 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -2,4 +2,14 @@
 Write a program that reads 10 numbers from
 the user and prints the largest one
 """
+# Exercise 1
+# By Zvi Figov
+print "Enter 10 positive numbers" # Assuming positive numbers 
+largest = -1
+for i in range(1,11):
+    print "Enter number %d" % i
+    num = int(raw_input())
+    largest = max(num,largest)
+
+print "Largest number is %d" % largest
 
