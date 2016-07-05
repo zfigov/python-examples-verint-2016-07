4ee7727e6af74a833eb029d8cdfb94d8c4c48c7c
zfigov <zfigov@gmail.com>
2016-07-05 10:14:14 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -5,4 +5,19 @@ After the user typed in an empty line,
 print all previously inserted lines in reverse
 order (from last to first)
 """
+# Exercise 4
+# By Zvi Figov
+
+
+
+total_lines = ""
+print "Enter text, empty line to stop"
+
+line = raw_input()
+while len(line)>0:
+    print "Input line %s" % line
+    total_lines = line + "\n" + total_lines
+    line = raw_input()
+
+print "Lines reversed: \n%s" % total_lines
 
