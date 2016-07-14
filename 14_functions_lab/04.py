2853016a947c110b0164a7736def5fe5896e0dca
zfigov <zfigov@gmail.com>
2016-07-14 13:41:14 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -4,3 +4,9 @@ a list of words, and returns only the words
 longer than minlen
 """
 
+def min_word_length(min_word_len,*words):
+    max_words = [word for word in words if len(word) > min_word_len]
+    return max_words
+
+print "Test min_word_length(5,'hi','dontgo',bye') "
+print min_word_length(5,'hi','dontgo','bye')
