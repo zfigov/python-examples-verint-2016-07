b737bb6efec5cbdb51e1d9a2d99bf48f6d70bd62
zfigov <zfigov@gmail.com>
2016-07-14 13:41:11 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -3,3 +3,9 @@ Write a function that calculates the sum
 of the 10th digit from all arguments passed to it
 """
 
+def sum_tens_digit(*numbers):
+
+    val = sum([int(str(n)[-2]) for n in numbers])
+    return val
+
+print "Test: sum_tens_digit(1120,220,140) = %d" % sum_tens_digit(1120,220,140)
