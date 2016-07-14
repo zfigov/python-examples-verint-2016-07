9c37942c1b37576fe1b802ddbcc05d78c3cadbd7
zfigov <zfigov@gmail.com>
2016-07-14 12:11:09 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -5,3 +5,39 @@ reads the file and searches for words with the same letters.
 All words with the same letters should be printed together
 """
 
+import sys
+from collections import Counter
+(_,file1) = sys.argv
+
+lines_file1 = []
+words = []
+
+# read all lines
+with open(file1,"r") as fin1:
+    lines_file1   = fin1.readlines()
+
+# create words list (wthout the endline)
+words = [ line.rstrip('\n') for line in lines_file1]
+
+# count the number of letters in each word
+words_count = []
+for word in words:
+    w = Counter(word)
+    words_count.append(w)
+
+# compare each word to the other using the counter
+found = [False for i in range(len(words))]
+
+for i in range(len(words)):
+    for j in range(i+1,len(words)):
+        if (words_count[i]==words_count[j]) :
+            print "%s  %s" %  (words[i],words[j])
+            found[i] = True
+            found[j] = True
+    if not found[i]:
+        print "%s " % (words[i])
+            
+
+
+
+
