06aaa348c1f192503d73258048e2eed802afee33
zfigov <zfigov@gmail.com>
2016-07-14 13:41:07 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -4,4 +4,17 @@ Write a function that takes two arguments:
     And a number
 If wrong types were passed in, raise an exception
 """
+from types import *
+
+def func(my_num,my_str):
+    if type(my_num) is not IntType  or type(my_str) is not StringType:
+        raise Exception("Error type")
+    else:
+        print "Input ok"
+
+# Test : 
+func(12,'ere')
+
+# Test : 
+func('23432','ere')
 
