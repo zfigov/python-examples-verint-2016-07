1ca34dd437ac04edcf42bea058a7e16fbc7a42c0
zfigov <zfigov@gmail.com>
2016-07-14 13:41:04 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -4,4 +4,30 @@ Write 2 functions:
     mymul - returns the multiplication of its input arguments
     Ignore non-numeric arguments
 """
+import operator 
 
+def mysum(*paramsin):
+    res = 0
+    res = sum([int(val) for val in paramsin if type(val) is int])
+    
+    return res
+
+def mymul(*paramsin):   
+    if len(paramsin)>0:
+        res = reduce(operator.mul,[int(val) for val in paramsin if type(val) is int])
+    else:
+        res = 1
+
+    return res
+
+# returns 60
+print "Test - mysum(10,20,30) = %d" % mysum(10,20,30)
+
+# returns 30
+print "Test - mysum(10,'foo',20) = %d" % mysum(10,'foo',20)
+
+# returns 1
+print "Test - mymul() = %d" % mymul() 
+
+# returns 200
+print "Test - mymul('foo','bar',10,20) = %d" % mymul('foo','bar',10,20) 
