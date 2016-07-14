f7383565c2de40f28cb54dc52cb06878f3527f74
zfigov <zfigov@gmail.com>
2016-07-14 12:44:29 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,27 @@
 """
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
+A file named hosts holds hostnames and IP addresses
+in format: hostname=ip
+Write a program that reads the file and takes
+a list of hostnames in sys.argv
+Program should print the IP addresses of the hosts requested
 """
+import sys
+from itertools import izip
+
+(_,file1) = sys.argv
+
+host_ip = {}
+with open(file1) as fin:
+    a = [(line.rstrip('\n')).split("=") for line in fin]
+
+for c1 in range(len(a)):
+    host_ip[a[c1][0]] = a[c1][1]
+
+print ("Enter username:")
+line = raw_input()
+
+if not (host_ip.has_key(line)):
+    print "Key not found"
+else:
+    print "Key found: %s" % host_ip[line]
+
