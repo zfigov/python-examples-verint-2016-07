df265b74ef3cc56cd1ecf2f707a52ac41cf65aeb
zfigov <zfigov@gmail.com>
2016-07-19 17:12:51 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -10,3 +10,17 @@ Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
 
+import sys
+import re
+
+(_,file1,key) = sys.argv
+
+
+lines_file1 = []
+
+with open(file1,"r") as fin1:
+    for line in fin1:
+        res = re.search(r'\b%s' % key,line)    
+        if res is not None:
+            (a,b) =  ((line[res.start()+len(key):]).split('='))
+            print b.strip(' \n')       
