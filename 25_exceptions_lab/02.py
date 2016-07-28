419f8042c59a7157e67775d81f92cb391093db30
zfigov <zfigov@gmail.com>
2016-07-28 17:26:34 +0300

    Solution for: sol-python-verint.exceptions-lab/02.py

--- 25_exceptions_lab/02.py
+++ 25_exceptions_lab/02.py
@@ -5,3 +5,16 @@ and prints line count for the file
 Alert the user politely if there was any problem opening the file
 """
 
+import sys
+import msvcrt
+
+(_,file1) = sys.argv
+
+try :
+    num_lines = sum(1 for line in open(file1))
+except Exception as e:
+    print 'Sorry %s file not found',file1
+    c = msvcrt.getch()
+    if c:
+        sys.exit()
+print 'Num lines : ',num_lines
